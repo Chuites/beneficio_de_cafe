0201039e9dd6302d0d98adbd5dacfbb70d9bcde3
chuites <carlosehs.9319@gmail.com>
2023-06-08 18:11:01 -0600

    Test Ruta

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::post('login', 'App\Http\Controllers\InicioController@login')->name("login
 
 Route::post('logout', 'App\Http\Controllers\InicioController@logout')->name("logout");
 Route::get('inicio', 'App\Http\Controllers\InicioController@index')->name("inicio");
-Route::post('generarPDF\{id_cargamento?}', 'App\Http\Controllers\InicioController@generarPDF')->name("generarPDF");
+Route::get('generarPDF\{id_cargamento?}', 'App\Http\Controllers\InicioController@generarPDF')->name("generarPDF");
 Route::post('listadoCargamentos', 'App\Http\Controllers\InicioController@listadoCargamentos')->name("listadoCargamentos");
 Route::post('listadoCargamentosReporte', 'App\Http\Controllers\InicioController@listadoCargamentosReporte')->name("listadoCargamentosReporte");
 Route::post('listadoPilotos', 'App\Http\Controllers\InicioController@listadoPilotos')->name("listadoPilotos");
